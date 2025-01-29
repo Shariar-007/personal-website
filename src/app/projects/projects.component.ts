b7c9ae57c08f10a4e7a9facc73498c66a15c7ff4
Shariar Shohag <mohammad.al-shariar@gim.com.bd>
2025-01-29 10:58:07 -0500

    added project section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,9 +12,44 @@ export class ProjectsComponent implements OnInit {
   constructor(private projectsService: ProjectsService) {}
 
   public ngOnInit() {
-    this.projectsService.getApiProjects().subscribe(
-      (res) => (this.githubProjects = res),
-      (err) => console.error(err)
-    );
+    // this.projectsService.getApiProjects().subscribe(
+    //   (res) => (this.githubProjects = res),
+    //   (err) => console.error(err)
+    // );
+    this.githubProjects.push(
+        {
+      html_url: 'https://github.com/Shariar-007/doctor-appointment-booking',
+      language:'Angular',
+      name:'Doctor Appointment Booking',
+      description:'This is a small project in which a patient can select specific time and book an appointment with filling a form with specific information'
+    },{
+          html_url: 'https://github.com/Shariar-007/Task-management-system/tree/main/task-management-system',
+          language:'Angular',
+          name:'Task Management System Like Jira',
+          description:'This just a small project with some features like JIRA'
+        },{
+            html_url: 'https://github.com/Shariar-007/Truck-stands-tracking-using-map/tree/master/truck-stands-tracking',
+            language:'Angular',
+            name:'Truck Stand Tracking Using MAP',
+            description:'Using google map tracking a vehicle'
+        },{
+            html_url: 'https://github.com/Shariar-007/book-store-management',
+            language:'Angular',
+            name:'Book Store Management System',
+            description:'Sore a list of books and mark your favourite books'
+        },
+       {
+          html_url: 'https://github.com/Shariar-007/spring-boot_fundamentals/tree/master/blog-app-apies',
+              language:'Java',
+          name:'Blog App APIs',
+          description:'Having apis of most of the features of blog website'
+      },
+        {
+            html_url: 'https://github.com/Shariar-007/spring-boot_fundamentals/tree/master/examserver',
+            language:'Java',
+            name:'Online Exam Site',
+            description:'Anyone can give subject wise online mcq exam'
+        }
+        )
   }
 }
